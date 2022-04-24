712806f4e07304be1e388dfdba92709016cffe24
Wojciech Sikora <wsikora@vuestorefront.io>
2022-04-24 11:49:47 +0200

    feat: add heading2 and heading3 components

---
+++ components/Heading2.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>
+    <RichText v-for="richText in data?.rich_text" :rich-text="richText" />
+  </h2>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+import { Heading } from "../types";
+import RichText from "./RichText.vue";
+defineProps({
+  data: Object as PropType<Heading>,
+});
+</script>
