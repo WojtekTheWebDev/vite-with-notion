6358dac930c831f1d6ce3699eaaacd3ab32dfecd
Wojciech Sikora <35867383+WojtekTheWebDev@users.noreply.github.com>
2022-04-24 11:50:41 +0200

    feat: add heading2 and heading3 components (#2)

--- components/Content.vue
+++ components/Content.vue
@@ -1,6 +1,8 @@
 <template>
   <section v-for="block in results">
     <Heading1 v-if="block.type === 'heading_1'" :data="block.heading_1" />
+    <Heading2 v-if="block.type === 'heading_2'" :data="block.heading_2" />
+    <Heading3 v-if="block.type === 'heading_3'" :data="block.heading_3" />
     <Paragraph v-if="block.type === 'paragraph'" :data="block.paragraph" />
     <ToDo v-if="block.type === 'to_do'" :data="block.to_do" />
   </section>
@@ -11,8 +13,11 @@ import { computed } from "@vue/reactivity";
 import { PropType } from "vue";
 import { Content } from "../types";
 import Heading1 from "./Heading1.vue";
+import Heading2 from "./Heading2.vue";
+import Heading3 from "./Heading3.vue";
 import Paragraph from "./Paragraph.vue";
 import ToDo from "./ToDo.vue";
+
 
 const props = defineProps({
   content: Object as PropType<Content>,
